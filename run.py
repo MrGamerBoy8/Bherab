64362a5fc5a32f09d7f6c56a0a2535362ab5c744
MrGamerBoy8 <118831753+MrGamerBoy8@users.noreply.github.com>
2023-07-29 18:03:24 +0545

    Add files via upload

---
+++ run.py
@@ -0,0 +1,8 @@
+import marshal
+from pytransform import pyarmor_runtime
+
+pyarmor_runtime()
+
+print("v6nom#7962")
+print("Cracked by COC (svenskithesource)")
+exec(marshal.loads(open("cracked_nightly", "rb").read()))
